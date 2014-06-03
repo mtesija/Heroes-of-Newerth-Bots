106b0b12755beee54a52a93bcb44a4a64e7e8283
Michael Tesija <mtesija@umich.edu>
2014-06-03 05:36:08 -0400

    Update arachna_main.lua

--- bots/arachna/arachna_main.lua
+++ bots/arachna/arachna_main.lua
@@ -1,15 +1,18 @@
 --ArachnaBot v1.0
 
+--------------------------------------
+--        Bot Initialization        --
+--------------------------------------
 
 local _G = getfenv(0)
 local object = _G.object
 
 object.myName = object:GetName()
 
-object.bRunLogic 		= true
+object.bRunLogic 	= true
 object.bRunBehaviors	= true
-object.bUpdates 		= true
-object.bUseShop 		= true
+object.bUpdates 	= true
+object.bUseShop 	= true
 
 object.bRunCommands 	= true
 object.bMoveCommands 	= true
@@ -17,9 +20,9 @@ object.bAttackCommands 	= true
 object.bAbilityCommands = true
 object.bOtherCommands 	= true
 
-object.bReportBehavior = false
-object.bDebugUtility = false
-object.bDebugExecute = false
+object.bReportBehavior	= false
+object.bDebugUtility	= false
+object.bDebugExecute	= false
 
 
 object.logger = {}
@@ -50,195 +53,16 @@ local Clamp = core.Clamp
 
 BotEcho('loading arachna_main...')
 
+-----------------------------
+--        Constants        --
+-----------------------------
+
+-- Arachna
 object.heroName = 'Hero_Arachna'
 
---------------------------------
--- Lanes
---------------------------------
-core.tLanePreferences = {Jungle = 0, Mid = 5, ShortSolo = 4, LongSolo = 3, ShortSupport = 1, LongSupport = 1, ShortCarry = 5, LongCarry = 4}
-
---------------------------------
--- Skills
---------------------------------
-function object:SkillBuild()
---Arachna specific
-	local unitSelf = self.core.unitSelf
-
-	if skills.webbedShot == nil then
-		skills.webbedShot = unitSelf:GetAbility(0)
-		skills.hardenCarapace = unitSelf:GetAbility(1)
-		skills.precision = unitSelf:GetAbility(2)
-		skills.spiderSting = unitSelf:GetAbility(3)
-		skills.attributeBoost = unitSelf:GetAbility(4)
-	end
-		
-	if unitSelf:GetAbilityPointsAvailable() <= 0 then
-		return
-	end
-	
-	--speicific level ordering first {precision, web, web, harden}
-	if not (skills.precision:GetLevel() >= 1) then
-		skills.precision:LevelUp()
-	elseif not (skills.webbedShot:GetLevel() >= 2) then
-		skills.webbedShot:LevelUp()
-	elseif not (skills.hardenCarapace:GetLevel() >= 1) then
-		skills.hardenCarapace:LevelUp()
-	--max in this order {ult, web, precision, carapace, stats}
-	elseif skills.spiderSting:CanLevelUp() then
-		skills.spiderSting:LevelUp()
-	elseif skills.webbedShot:CanLevelUp() then
-		skills.webbedShot:LevelUp()
-	elseif skills.precision:CanLevelUp() then
-		skills.precision:LevelUp()
-	elseif skills.hardenCarapace:CanLevelUp() then
-		skills.hardenCarapace:LevelUp()
-	else
-		skills.attributeBoost:LevelUp()
-	end	
-end
-
----------------------------------------------------
---                   Overrides                   --
----------------------------------------------------
-
-----------------------------------
---	Arachna specific harass bonuses
---
---  Abilities off cd increase harass util
---  Ability use increases harass util for a time
-----------------------------------
-
-object.webUpBonus = 5
-object.spiderUpBonus = 20
-object.spiderUseBonus = 45
-
-local function AbilitiesUpUtilityFn()
-	local val = 0
-	
-	if skills.webbedShot:CanActivate() then
-		val = val + object.webUpBonus
-	end
-	
-	if skills.spiderSting:CanActivate() then
-		val = val + object.spiderUpBonus
-	end
-	
-	return val
-end
-
---Arachna ability use gives bonus to harass util for a while
-function object:oncombateventOverride(EventData)
-	self:oncombateventOld(EventData)
-	
-	local addBonus = 0
-	
-	if EventData.Type == "Ability" then
-		--BotEcho("ABILILTY EVENT!  InflictorName: "..EventData.InflictorName)		
-		if EventData.InflictorName == "Ability_Arachna4" then
-			addBonus = addBonus + object.spiderUseBonus
-		end
-	end
-	
-	if addBonus > 0 then
-		--decay before we add
-		core.DecayBonus(self)
-	
-		core.nHarassBonus = core.nHarassBonus + addBonus
-	end
-end
-object.oncombateventOld = object.oncombatevent
-object.oncombatevent 	= object.oncombateventOverride
-
---Util override
-local function CustomHarassUtilityOverride(hero)
-	local nUtility = AbilitiesUpUtilityFn()
-	
-	return nUtility
-end
-behaviorLib.CustomHarassUtility = CustomHarassUtilityOverride   
-
-----------------------------------
---	Arachna specific push strength
-----------------------------------
-local function PushingStrengthUtilOverride(myHero)
-	local myDamage = core.GetFinalAttackDamageAverage(myHero)
-	local myAttackDuration = myHero:GetAdjustedAttackDuration()
-	local myDPS = myDamage * 1000 / (myAttackDuration) --ms to s
-	
-	local vTop = Vector3.Create(300, 100)
-	local vBot = Vector3.Create(100, 0)
-	local m = ((vTop.y - vBot.y)/(vTop.x - vBot.x))
-	local b = vBot.y - m * vBot.x 
-	
-	local util = m * myDPS + b
-	util = Clamp(util, 0, 100)
-	
-	--BotEcho(format("MyDPS: %g  util: %g  myMin: %g  myMax: %g  myAttackAverageL %g", 
-	--	myDPS, util, myHero:GetFinalAttackDamageMin(), myHero:GetFinalAttackDamageMax(), myDamage))
-
-	return util
-end
-behaviorLib.PushingStrengthUtilFn = PushingStrengthUtilOverride
-
-
-----------------------------------
---	Arachna harass actions
-----------------------------------
-local function HarassHeroExecuteOverride(botBrain)
-	local unitTarget = behaviorLib.heroTarget
-	if unitTarget == nil or not unitTarget:IsValid() then
-		return false --can not execute, move on to the next behavior
-	end
-	
-	local unitSelf = core.unitSelf
-	
-	local bActionTaken = false
-	
-	--since we are using an old pointer, ensure we can still see the target for entity targeting
-	if core.CanSeeUnit(botBrain, unitTarget) then
-		local dist = Vector3.Distance2D(unitSelf:GetPosition(), unitTarget:GetPosition())
-		local attkRange = core.GetAbsoluteAttackRangeToUnit(unitSelf, unitTarget);
-		
-		local itemGhostMarchers = core.itemGhostMarchers
-		
-		local sting = skills.spiderSting
-		local stingRange = sting and (sting:GetRange() + core.GetExtraRange(unitSelf) + core.GetExtraRange(unitTarget)) or 0
-		local web = skills.webbedShot
-
-		local bUseSting = true
-		if core.nDifficulty == core.nEASY_DIFFICULTY and not unitTarget:IsBotControlled() then
-			bUseSting = false
-		end
-		
-		if sting and sting:CanActivate() and bUseSting and dist < stingRange then
-			bActionTaken = core.OrderAbilityEntity(botBrain, sting, unitTarget)
-		elseif dist < attkRange and unitSelf:IsAttackReady() and web and web:CanActivate() then
-			bActionTaken = core.OrderAbilityEntity(botBrain, web, unitTarget)
-		elseif (sting and sting:CanActivate() and bUseSting and dist > stingRange) then
-			--move in when we want to ult			
-			local desiredPos = unitTarget:GetPosition()
-			
-			if itemGhostMarchers and itemGhostMarchers:CanActivate() then
-				bActionTaken = core.OrderItemClamp(botBrain, unitSelf, itemGhostMarchers)
-			end
-			
-			if not bActionTaken and behaviorLib.lastHarassUtil < behaviorLib.diveThreshold then
-				desiredPos = core.AdjustMovementForTowerLogic(desiredPos)
-			end
-			core.OrderMoveToPosClamp(botBrain, unitSelf, desiredPos, false)
-			bActionTaken = true
-		end
-	end
-	
-	if not bActionTaken then
-		return object.harassExecuteOld(botBrain)
-	end
-end
-object.harassExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
-behaviorLib.HarassHeroBehavior["Execute"] = HarassHeroExecuteOverride
-
-
-behaviorLib.StartingItems = {"2 Item_DuckBoots", "2 Item_MinorTotem", "Item_HealthPotion", "Item_RunesOfTheBlight"}
+-- Item buy order, using internal names.
+behaviorLib.StartingItems = 
+	{"2 Item_DuckBoots", "2 Item_MinorTotem", "Item_HealthPotion", "Item_RunesOfTheBlight"}
 behaviorLib.LaneItems = 
 	{"Item_Marchers", "2 Item_Soulscream", "Item_EnhancedMarchers"}
 behaviorLib.MidItems = 
@@ -248,27 +72,173 @@ behaviorLib.LateItems =
 	{"Item_Weapon3", "Item_Evasion", "Item_BehemothsHeart", "Item_Damage9" } 
 	--Weapon3 is Savage Mace, Item_Evasion is Wingbow, and Item_Damage9 is Doombringer
 
+-- Skillbuild table, 0 = q, 1 = w, 2 = e, 3 = r, 4 = attributes
+object.tSkills = {
+	2, 0, 0, 1, 0,
+	3, 0, 2, 2, 2,
+	3, 1, 1, 1, 4, 
+	3, 4, 4, 4, 4,
+	4, 4, 4, 4, 4
+}
 
---[[ colors:
-	red
-	aqua == cyan
-	gray
-	navy
-	teal
-	blue
-	lime
-	black
-	brown
-	green
-	olive
-	white
-	silver
-	purple
-	maroon
-	yellow
-	orange
-	fuchsia == magenta
-	invisible
---]]
+-- Lane preferences
+core.tLanePreferences = {
+	Jungle = 0, 
+	Mid = 5, 
+	ShortSolo = 4, 
+	LongSolo = 3, 
+	ShortSupport = 1, 
+	LongSupport = 1, 
+	ShortCarry = 5, 
+	LongCarry = 4
+}
+
+-- Bonus agression points if a skill/item is available for use
+object.nWebUp = 5
+object.nSpiderUp = 20
+
+-- Bonus agression points that are applied to the bot upon successfully using a skill/item
+object.spiderUseBonus = 45
+
+--------------------------
+--        Skills        --
+--------------------------
+
+function object:SkillBuild()
+	local unitSelf = self.core.unitSelf
+
+	if skills.abilWebbedShot == nil then
+		skills.abilWebbedShot = unitSelf:GetAbility(0)\
+		skills.abilSpiderSting = unitSelf:GetAbility(3)
+	end
+		
+	local nPoints = unitSelf:GetAbilityPointsAvailable()
+	if nPoints <= 0 then
+		return
+	end
+ 
+	local nLevel = unitSelf:GetLevel()
+	for i = nLevel, (nLevel + nPoints) do
+		unitSelf:GetAbility( self.tSkills[i] ):LevelUp()
+	end	
+end
+
+------------------------------------------
+--        OnCombatEvent Override        --
+------------------------------------------
+
+function object:oncombateventOverride(EventData)
+	self:oncombateventOld(EventData)
+	
+	local nAddBonus = 0
+	
+	if EventData.Type == "Ability" then	
+		if EventData.InflictorName == "Ability_Arachna4" then
+			nAddBonus = nAddBonus + object.spiderUseBonus
+		end
+	end
+	
+	if nAddBonus > 0 then
+		core.DecayBonus(self)
+	
+		core.nHarassBonus = core.nHarassBonus + nAddBonus
+	end
+end
+
+object.oncombateventOld = object.oncombatevent
+object.oncombatevent = object.oncombateventOverride
+
+-----------------------------------------------
+--        CustomHarassUtilityOverride        --
+-----------------------------------------------
+
+local function CustomHarassUtilityOverride()
+	local nUtility = 0
+	
+	if skills.abilWebbedShot:CanActivate() then
+		nUtility = nUtility + object.nWebUp
+	end
+	
+	if skills.abilSpiderSting:CanActivate() then
+		nUtility = nUtility + object.nSpiderUp
+	end
+	
+	return nUtility
+end
+
+behaviorLib.CustomHarassUtility = CustomHarassUtilityOverride   
+
+----------------------------------------
+--        PushStrengthOverride        --
+----------------------------------------
+
+local function PushingStrengthUtilOverride(unitSelf)
+	local nMyDPS = core.GetFinalAttackDamageAverage(unitSelf) * 1000 / unitSelf:GetAdjustedAttackDuration()
+	
+	local nUtility = 0.5 * nMyDPS - 50
+	nUtility = Clamp(nUtility, 0, 100)
+
+	return nUtility
+end
+
+behaviorLib.PushingStrengthUtilFn = PushingStrengthUtilOverride
+
+-----------------------------------
+--        Harass Behavior        --
+-----------------------------------
+
+local function HarassHeroExecuteOverride(botBrain)
+	local unitTarget = behaviorLib.heroTarget
+	
+	if unitTarget == nil then
+		return false
+	end
+	
+	if core.CanSeeUnit(botBrain, unitTarget) then
+		
+		local bActionTaken = false
+		
+		local unitSelf = core.unitSelf
+		local vecTargetPosition = unitTarget:GetPosition()
+		local nTargetDistanceSq = Vector3.Distance2DSq(unitSelf:GetPosition(), vecTargetPosition)
+		
+		local abilSting = skills.abilSpiderSting
+		if abilSting and abilSting:CanActivate() and core.nDifficulty ~= core.nEASY_DIFFICULTY or unitTarget:IsBotControlled() then
+			local nStingRange = abilSting:GetRange() + core.GetExtraRange(unitSelf) + core.GetExtraRange(unitTarget)
+			if nTargetDistanceSq < nStingRange * nStingRange then
+				bActionTaken = core.OrderAbilityEntity(botBrain, abilSting, unitTarget)
+			else
+				local itemGhostMarchers = core.itemGhostMarchers
+				if itemGhostMarchers and itemGhostMarchers:CanActivate() then
+					core.OrderItemClamp(botBrain, unitSelf, itemGhostMarchers)
+				end
+			
+				vecDesiredPos = vecTargetPosition
+				if behaviorLib.lastHarassUtil < behaviorLib.diveThreshold then
+					vecDesiredPos = core.AdjustMovementForTowerLogic(vecDesiredPos)
+				end
+			
+				bActionTaken = core.OrderMoveToPosClamp(botBrain, unitSelf, desiredPos, false)	
+			end
+		end
+		
+		if not bActionTaken then
+			local abilWeb = skills.abilWebbedShot
+			if abilWeb and abilWeb:CanActivate() and unitSelf:IsAttackReady() then
+				local nWebRange = abilWeb:GetRange() + core.GetExtraRange(unitSelf) + core.GetExtraRange(unitTarget)
+				if nTargetDistanceSq < nWebRange * nWebRange then
+					bActionTaken = core.OrderAbilityEntity(botBrain, abilWeb, unitTarget)
+				end
+			end
+		end
+	end
+	
+	if not bActionTaken then
+		return object.harassExecuteOld(botBrain)
+	end
+end
+
+object.harassExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
+behaviorLib.HarassHeroBehavior["Execute"] = HarassHeroExecuteOverride
 
 BotEcho('finished loading arachna_main')
